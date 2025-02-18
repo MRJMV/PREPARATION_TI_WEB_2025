86dad97fd1977be80ed04979f9314c58114569dc
JsnajhM <earn_arctic.0k@icloud.com>
2025-02-18 15:26:41 +0100

    formulaire

--- templates/contactView.php
+++ templates/contactView.php
@@ -16,18 +16,22 @@
     </header>
     <section>
         <div>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
-            </p>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
-            </p>
+            <form action="">
+                <label for="fname">First Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name..">
+
+                <label for="lname">Last Name</label>
+                <input type="text" id="lname" name="lastname" placeholder="Your last name..">
+
+                <label for="country">Country</label>
+                <select id="country" name="country">
+                    <option value="Belgique">Belgique</option>
+                    <option value="Pays-Bas">Pays-Bas</option>
+                    <option value="Allemagne">Allemagne</option>
+                </select>
+
+                <input type="submit" value="Submit">
+            </form>
         </div>
     </section>
     <footer>

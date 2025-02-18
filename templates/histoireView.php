0d43e9a2a0bedf81648d24df1dda1c0c47bd2742
JsnajhM <earn_arctic.0k@icloud.com>
2025-02-18 15:20:32 +0100

    ajout d'images dans histoire

--- templates/histoireView.php
+++ templates/histoireView.php
@@ -16,12 +16,15 @@
     </header>
     <section>
         <div>
+            <img src="img/a.jpeg">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
             </p>
+            <img src="img/b.jpeg">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
             </p>
+            <img src="img/c.jpeg">
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam id dicta sit minima. Libero sequi assumenda aut? Excepturi sequi officia illum quasi amet rem explicabo enim quibusdam ad at magnam labore tenetur, corrupti nobis eligendi mollitia illo, adipisci reprehenderit? Asperiores natus, doloremque et facere similique suscipit consequuntur repellendus sit temporibus atque, nam unde cumque cum laborum dicta obcaecati tenetur nemo accusamus ipsa quo alias dolorem sapiente aliquam labore? At aperiam maxime praesentium saepe consequatur quos voluptatibus vel odit dolores accusamus illo, impedit officiis quidem accusantium asperiores doloremque, sint distinctio voluptates aliquam voluptatem. Voluptas assumenda corrupti, harum soluta possimus alias fugit.
             </p>
